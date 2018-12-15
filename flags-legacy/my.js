3e67f2e774b6e0e0f199d684e9dea33d1d0eedc1
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2018-12-15 15:15:06 +0100

    created vue component

--- flags-legacy/my.js
+++ flags-legacy/my.js
@@ -6,30 +6,20 @@ function getRandomCountry(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-var app = new Vue({
-  el: "#app", //element im dom/html an den man sich verbinden will (ist die id)
-  data: {
-      country: 'de'
-  },
-  methods: {
-    changeCountry: function(){
-        this.country = countries[getRandomCountry(countries.length)];
+Vue.component('vue-flag-icon', {
+  template: `
+    <div>
+    <link href="./assets/flags.css" rel=stylesheet type="text/css">
+    <img id="flag-image" src="./assets/blank.gif" v-bind:class="css" />
+    </div> `,
+    props: ['country'],
+    data: function(){
+        return {
+          css: 'flag flag-' + this.country
+        }
     }
-  },
-  mounted(){
-    console.log("mounted");
-  },
-  updated: function() {
-    console.log("updated");
-  },
-});
+})
 
-app.$watch("country", function(newValue, oldValue) {
-  console.log("watcher: new: " + newValue + " | old: " + oldValue);
-});
-
-app.$watch("country", (newValue, oldValue) =>
-  console.log("one line watcher: new: " + newValue + " | old: " + oldValue)
-);
-
-//mit npm install vue vermutlich einbinden
+var app = new Vue({
+  el: '#app'
+})
