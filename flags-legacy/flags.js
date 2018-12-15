6447f3e8fdd6ccac616431696ff8e44a46cf6170
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2018-12-15 11:46:20 +0100

    added

--- flags-legacy/flags.js
+++ flags-legacy/flags.js
@@ -1,24 +1,30 @@
 class FlagIcon extends HTMLElement {
-    constructor(){
-        super();
-        this._ShadowRoot = this.attachShadow({
-            mode : 'open'
-        });
-        this._ShadowRoot.innerHTML  = `
+  constructor() {
+    super();
+    this._ShadowRoot = this.attachShadow({
+      mode: "open"
+    });
+    this._ShadowRoot.innerHTML = `
             <link href="./assets/flags.css" rel=stylesheet type="text/css">
             <img id="flag-image" src="./assets/blank.gif" class="flag" />
-        `
-    }
+        `;
+  }
 
-    connectedCallback(){ //lifecyle hookc
-        this.updateFlag(this.getAttribute('country'))
-    }
+  connectedCallback() {
+    //lifecyle hookc
+    this.updateFlag(this.getAttribute("country"));
+  }
 
-    updateFlag(country){
-        this._img = this._ShadowRoot.querySelector("#flag-image")
-        this._img.className = 'flag flag-' + country
-    }
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log("old value: " + oldValue + " | new value: " + newValue);
+    this.updateFlag(newValue);
+  }
+
+  updateFlag(country) {
+    this._img = this._ShadowRoot.querySelector("#flag-image");
+    this._img.className = "flag flag-" + country;
+  }
 }
 
-customElements.define('flag-icon', FlagIcon) 
+customElements.define("flag-icon", FlagIcon);
 //web components | immer wenn flag-icon verwendet wird soll FlagIcon Klasse verwendet werden
