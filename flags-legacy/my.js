e8df8c116df9cfea37d312f3f0b933033eceb1e4
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2018-12-15 14:28:09 +0100

    vue error/warning ausgeschaltet

--- flags-legacy/my.js
+++ flags-legacy/my.js
@@ -1,23 +1,25 @@
-
+Vue.config.ignoredElements = ['flag-icon']
 
 var app = new Vue({
-    el: '#app', //element im dom/html an den man sich verbinden will (ist die id)
-    data: {
-        country: 'gr',
-    },
-    mounted() {
-        console.log('mounted');
-    },
-    updated(){
-        console.log('updated')
-    }
-})
+  el: "#app", //element im dom/html an den man sich verbinden will (ist die id)
+  data: {
+    country: "gr"
+  },
+  mounted: function() {
+    console.log("mounted");
+  },
+  updated: function() {
+    console.log("updated");
+  },
+  watch: {}
+});
 
-app.$watch('country', function(newValue, oldValue){
-    console.log("watcher: new: " + newValue + " | old: " + oldValue)
-})
+app.$watch("country", function(newValue, oldValue) {
+  console.log("watcher: new: " + newValue + " | old: " + oldValue);
+});
 
-app.$watch('country', (newValue, oldValue) => 
-console.log("one line watcher: new: " + newValue + " | old: " + oldValue))
+app.$watch("country", (newValue, oldValue) =>
+  console.log("one line watcher: new: " + newValue + " | old: " + oldValue)
+);
 
 //mit npm install vue vermutlich einbinden
