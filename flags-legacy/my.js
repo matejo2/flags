33d38c915f14e3b5cdcf237a858b9c4355c15aa5
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2018-12-15 13:34:41 +0100

    view binding with flags

---
+++ flags-legacy/my.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app', //element im dom/html an den man sich verbinden will (ist die id)
+    data: {
+        country: 'ro',
+    }
+})
+
+//mit npm install vue vermutlich einbinden
