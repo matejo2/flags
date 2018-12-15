f094f82efaa6e2e81bf11e5274e97efdfdedd4e3
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2018-12-15 11:40:07 +0100

    first webcomponent

--- flags-legacy/flags.js
+++ flags-legacy/flags.js
@@ -1,12 +1,12 @@
-class FlagIcon extends HTMLElement () {
+class FlagIcon extends HTMLElement {
     constructor(){
         super();
         this._ShadowRoot = this.attachShadow({
             mode : 'open'
         });
         this._ShadowRoot.innerHTML  = `
-            <link href="assets/flags.css" rel=stylesheet type="type/css">
-            <img id="flag-image" src="assets/blank.gif" class="flag" />
+            <link href="./assets/flags.css" rel=stylesheet type="text/css">
+            <img id="flag-image" src="./assets/blank.gif" class="flag" />
         `
     }
 
